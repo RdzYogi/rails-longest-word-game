e769bf9d934b79bd8ed972c7053927f4db5ef8a8
Silviu Rosu <silviurosu1988@gmail.com>
2022-11-14 16:23:09 +0100

    refactored controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,19 +5,14 @@ class GamesController < ApplicationController
   def board
     session[:grid] = ('A'..'Z').to_a.sample(10)
     @grid = session[:grid]
+    session[:tscore] = 0 if params["refresh"] == "true"
   end
 
   def score
-    url = "https://wagon-dictionary.herokuapp.com/#{params['attempt']}"
-    response_serialized = URI.open(url).read
-    response = JSON.parse(response_serialized)
-    included_check = true
+    response = check_word(params['attempt'])
     @grid = session[:grid]
     @total_score = session[:tscore] || 0
-    params['attempt'].each_char do |char|
-      included_check &&= @grid.include?(char.upcase)
-      @grid.delete_at(@grid.index(char.upcase)) unless @grid.index(char.upcase).nil?
-    end
+    included_check = check_included_grid(params['attempt'], @grid)
     if response['found'] == false
       @result = { message: "Sorry, but #{params['attempt']} is not an english word", score: 0 }
     elsif included_check == false
@@ -25,10 +20,30 @@ class GamesController < ApplicationController
     else
       @result = { message: "#{params['attempt']} is an english word",
                   score: params['attempt'].size }
-      tscore = @total_score
-      tscore += @result[:score]
-      session[:tscore] = tscore
-      @total_score = session[:tscore] || 0
+      @total_score = update_score(@total_score, @result[:score])
     end
+  end
+
+  private
+
+  def update_score(oldtscore, newscore)
+    tscore = oldtscore + newscore
+    session[:tscore] = tscore
+    tscore
+  end
+
+  def check_included_grid(word, grid)
+    included_check = true
+    word.each_char do |char|
+      included_check &&= grid.include?(char.upcase)
+      grid.delete_at(grid.index(char.upcase)) unless grid.index(char.upcase).nil?
+    end
+    included_check
+  end
+
+  def check_word(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    response_serialized = URI.open(url).read
+    JSON.parse(response_serialized)
   end
 end
